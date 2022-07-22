930a4ee37c0814039f2f95a9fce78c65d4256ac7
Shabalin Sergey <sergeyshm38@gmail.com>
2022-07-22 10:27:30 +0300

    fix commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,42 +1,31 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
- public class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
 
     @Test
-    void ShouldViewIssueTest() {
+    public void ShouldReturnZero() {
 
         CashbackHackService service = new CashbackHackService();
-        int expected = 100;
+        int expected = 0;
         int amount = 1000;
         int actual = service.remain(amount);
 
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void ShouldBeOkay() {
+    public void ShouldReturnHundred() {
         CashbackHackService service = new CashbackHackService();
         int expected = 100;
         int amount = 900;
         int actual = service.remain(amount);
 
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
-
-     @Test
-     public void JustForInterest() {
-         CashbackHackService service = new CashbackHackService();
-         int expected = 500;
-         int amount = 1500;
-         int actual = service.remain(amount);
-
-         assertEquals(expected,actual);
-     }
-
 
 }
 
